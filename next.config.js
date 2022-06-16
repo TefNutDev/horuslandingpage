f4c144d64afcc12ef9b02bb75f2d7b81dc067586
TefNut <evanmeleady@icloud.com>
2022-06-16 22:26:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const withImages = require("next-images");
-
-module.exports = withImages({
+module.exports = {
   images: {
-    disableStaticImages: true,
-  },
-});
+    disableStaticImages: true
+  }
+}
 
